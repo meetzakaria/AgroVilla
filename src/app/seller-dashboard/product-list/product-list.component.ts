b10a70cc2052429751dc190f6e403454d0261611
Zakaria Hossen <zakariahossen65@gmail.com>
2025-05-16 13:10:18 +0600

    update

--- src/app/seller-dashboard/product-list/product-list.component.ts
+++ src/app/seller-dashboard/product-list/product-list.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { NavbarComponent } from "../../navbar/navbar.component";
 
 @Component({
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent],
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
 })
 export class ProductListComponent implements OnInit {
   products: any[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient , private router: Router) {}
 
   ngOnInit() {
     this.http.get<any[]>('http://localhost:8080/api/products')
       .subscribe(data => {
         this.products = data;
       });
+  }
+
+  onAddProduct() {
+    // Redirect to product form or trigger modal
+    this.router.navigate(['/ap']);
   }
 }
